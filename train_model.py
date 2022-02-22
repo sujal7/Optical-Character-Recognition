8ada0c61593703e893a05f7bf9c177ff0984b2e9
Sujal Duwa <sujalduwa@gmail.com>
2022-02-22 22:30:45 +0545

    Update: Extracted text properly from the output image

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
-
 from tensorflow.keras.datasets import mnist
 import numpy as np
+
 
 def load_az_dataset(datasetPath):
     # initialize the list of data and labels
@@ -30,6 +30,7 @@ def load_az_dataset(datasetPath):
     # return a 2-tuple of the A-Z data and labels
     return (data, labels)
 
+
 def load_mnist_dataset():
     # load the MNIST dataset and stack the training data and testing
     # data together (we'll create our own training and testing splits
@@ -41,11 +42,10 @@ def load_mnist_dataset():
     # return a 2-tuple of the MNIST data and labels
     return (data, labels)
 
-# USAGE
-# python train_resnet_model.py --az a_z_handwritten_data.csv --model handwriting.model
 
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
+
 matplotlib.use("Agg")
 
 # import the necessary packages
@@ -59,7 +59,6 @@ from sklearn.metrics import classification_report
 from imutils import build_montages
 import matplotlib.pyplot as plt
 import numpy as np
-import argparse
 import cv2
 
 # initialize the number of epochs to train for, initial learning rate,
@@ -70,7 +69,9 @@ BS = 128
 
 # load the A-Z and MNIST datasets, respectively
 print("[INFO] loading datasets...")
-(azData, azLabels) = load_az_dataset("a_z_handwritten_data.csv") # upload csv file in your drive and give path here
+(azData, azLabels) = load_az_dataset(
+    "a_z_handwritten_data.csv"
+)  # upload csv file in your drive and give path here
 (digitsData, digitsLabels) = load_mnist_dataset()
 
 # the MNIST dataset occupies the labels 0-9, so let's add 10 to every
@@ -104,22 +105,24 @@ classWeight = {}
 
 # loop over all classes and calculate the class weight
 for i in range(0, len(classTotals)):
-	classWeight[i] = classTotals.max() / classTotals[i]
+    classWeight[i] = classTotals.max() / classTotals[i]
 
 # partition the data into training and testing splits using 80% of
 # the data for training and the remaining 20% for testing
-(trainX, testX, trainY, testY) = train_test_split(data,
-	labels, test_size=0.20, stratify=labels, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(
+    data, labels, test_size=0.20, stratify=labels, random_state=42
+)
 
 # construct the image generator for data augmentation
 aug = ImageDataGenerator(
-	rotation_range=10,
-	zoom_range=0.05,
-	width_shift_range=0.1,
-	height_shift_range=0.1,
-	shear_range=0.15,
-	horizontal_flip=False,
-	fill_mode="nearest")
+    rotation_range=10,
+    zoom_range=0.05,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.15,
+    horizontal_flip=False,
+    fill_mode="nearest",
+)
 
 # import the necessary packages
 from tensorflow.keras.layers import BatchNormalization
@@ -136,41 +139,46 @@ from tensorflow.keras.layers import add
 from tensorflow.keras.regularizers import l2
 from tensorflow.keras import backend as K
 
+
 class CNN:
-    def residual_module(data, K, stride, chanDim, red=False,
-                        reg=0.0001, bnEps=2e-5, bnMom=0.9):
+    def residual_module(
+        data, K, stride, chanDim, red=False, reg=0.0001, bnEps=2e-5, bnMom=0.9
+    ):
         # the shortcut branch of the CNN module should be
         # initialize as the input (identity) data
         shortcut = data
 
         # the first block of the CNN module are the 1x1 CONVs
-        bn1 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                 momentum=bnMom)(data)
+        bn1 = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)(data)
         act1 = Activation("relu")(bn1)
-        conv1 = Conv2D(int(K * 0.25), (1, 1), use_bias=False,
-                       kernel_regularizer=l2(reg))(act1)
+        conv1 = Conv2D(
+            int(K * 0.25), (1, 1), use_bias=False, kernel_regularizer=l2(reg)
+        )(act1)
 
         # the second block of the CNN module are the 3x3 CONVs
-        bn2 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                 momentum=bnMom)(conv1)
+        bn2 = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)(conv1)
         act2 = Activation("relu")(bn2)
-        conv2 = Conv2D(int(K * 0.25), (3, 3), strides=stride,
-                       padding="same", use_bias=False,
-                       kernel_regularizer=l2(reg))(act2)
+        conv2 = Conv2D(
+            int(K * 0.25),
+            (3, 3),
+            strides=stride,
+            padding="same",
+            use_bias=False,
+            kernel_regularizer=l2(reg),
+        )(act2)
 
         # the third block of the CNN module is another set of 1x1
         # CONVs
-        bn3 = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                                 momentum=bnMom)(conv2)
+        bn3 = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)(conv2)
         act3 = Activation("relu")(bn3)
-        conv3 = Conv2D(K, (1, 1), use_bias=False,
-                       kernel_regularizer=l2(reg))(act3)
+        conv3 = Conv2D(K, (1, 1), use_bias=False, kernel_regularizer=l2(reg))(act3)
 
         # if we are to reduce the spatial size, apply a CONV layer to
         # the shortcut
         if red:
-            shortcut = Conv2D(K, (1, 1), strides=stride,
-                              use_bias=False, kernel_regularizer=l2(reg))(act1)
+            shortcut = Conv2D(
+                K, (1, 1), strides=stride, use_bias=False, kernel_regularizer=l2(reg)
+            )(act1)
 
         # add together the shortcut and the final CONV
         x = add([conv3, shortcut])
@@ -178,8 +186,18 @@ class CNN:
         # return the addition as the output of the CNN module
         return x
 
-    def build(width, height, depth, classes, stages, filters,
-              reg=0.0001, bnEps=2e-5, bnMom=0.9, dataset="cifar"):
+    def build(
+        width,
+        height,
+        depth,
+        classes,
+        stages,
+        filters,
+        reg=0.0001,
+        bnEps=2e-5,
+        bnMom=0.9,
+        dataset="cifar",
+    ):
         # initialize the input shape to be "channels last" and the
         # channels dimension itself
         inputShape = (height, width, depth)
@@ -193,28 +211,33 @@ class CNN:
 
         # set the input and then apply a BN followed by CONV
         inputs = Input(shape=inputShape)
-        x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                               momentum=bnMom)(inputs)
-        x = Conv2D(filters[0], (3, 3), use_bias=False,
-                   padding="same", kernel_regularizer=l2(reg))(x)
+        x = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)(inputs)
+        x = Conv2D(
+            filters[0],
+            (3, 3),
+            use_bias=False,
+            padding="same",
+            kernel_regularizer=l2(reg),
+        )(x)
 
         # loop over the number of stages
         for i in range(0, len(stages)):
             # initialize the stride, then apply a residual module
             # used to reduce the spatial size of the input volume
             stride = (1, 1) if i == 0 else (2, 2)
-            x = CNN.residual_module(x, filters[i + 1], stride,
-                                       chanDim, red=True, bnEps=bnEps, bnMom=bnMom)
+            x = CNN.residual_module(
+                x, filters[i + 1], stride, chanDim, red=True, bnEps=bnEps, bnMom=bnMom
+            )
 
             # loop over the number of layers in the stage
             for j in range(0, stages[i] - 1):
                 # apply a CNN module
-                x = CNN.residual_module(x, filters[i + 1],
-                                           (1, 1), chanDim, bnEps=bnEps, bnMom=bnMom)
+                x = CNN.residual_module(
+                    x, filters[i + 1], (1, 1), chanDim, bnEps=bnEps, bnMom=bnMom
+                )
 
         # apply BN => ACT => POOL
-        x = BatchNormalization(axis=chanDim, epsilon=bnEps,
-                               momentum=bnMom)(x)
+        x = BatchNormalization(axis=chanDim, epsilon=bnEps, momentum=bnMom)(x)
         x = Activation("relu")(x)
         x = AveragePooling2D((8, 8))(x)
 
@@ -228,23 +251,25 @@ class CNN:
         # return the constructed network architecture
         return model
 
+
 # initialize and compile our deep neural network
 print("[INFO] compiling model...")
 opt = SGD(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
-model = CNN.build(32, 32, 1, len(le.classes_), (3, 3, 3),
-	(64, 64, 128, 256), reg=0.0005)
-model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+model = CNN.build(
+    32, 32, 1, len(le.classes_), (3, 3, 3), (64, 64, 128, 256), reg=0.0005
+)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # train the network
 print("[INFO] training network...")
 H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	validation_data=(testX, testY),
-	steps_per_epoch=2,
-	epochs=10,
-	class_weight=classWeight,
-	verbose=1)
+    aug.flow(trainX, trainY, batch_size=BS),
+    validation_data=(testX, testY),
+    steps_per_epoch=2,
+    epochs=10,
+    class_weight=classWeight,
+    verbose=1,
+)
 
 # define the list of label names
 labelNames = "0123456789"
@@ -254,8 +279,11 @@ labelNames = [l for l in labelNames]
 # evaluate the network
 print("[INFO] evaluating network...")
 predictions = model.predict(testX, batch_size=BS)
-print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=labelNames))
+print(
+    classification_report(
+        testY.argmax(axis=1), predictions.argmax(axis=1), target_names=labelNames
+    )
+)
 # save the model to disk
 print("[INFO] serializing network...")
 model.save(["model"], save_format="h5")
@@ -276,29 +304,28 @@ images = []
 
 # randomly select a few testing characters
 for i in np.random.choice(np.arange(0, len(testY)), size=(49,)):
-	# classify the character
-	probs = model.predict(testX[np.newaxis, i])
-	prediction = probs.argmax(axis=1)
-	label = labelNames[prediction[0]]
+    # classify the character
+    probs = model.predict(testX[np.newaxis, i])
+    prediction = probs.argmax(axis=1)
+    label = labelNames[prediction[0]]
 
-	# extract the image from the test data and initialize the text
-	# label color as green (correct)
-	image = (testX[i] * 255).astype("uint8")
-	color = (0, 255, 0)
+    # extract the image from the test data and initialize the text
+    # label color as green (correct)
+    image = (testX[i] * 255).astype("uint8")
+    color = (0, 255, 0)
 
-	# otherwise, the class label prediction is incorrect
-	if prediction[0] != np.argmax(testY[i]):
-		color = (0, 0, 255)
+    # otherwise, the class label prediction is incorrect
+    if prediction[0] != np.argmax(testY[i]):
+        color = (0, 0, 255)
 
-	# merge the channels into one image, resize the image from 32x32
-	# to 96x96 so we can better see it and then draw the predicted
-	# label on the image	image = cv2.merge([image] * 3)
-	image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_LINEAR)
-	cv2.putText(image, label, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
-		color, 2)
+    # merge the channels into one image, resize the image from 32x32
+    # to 96x96 so we can better see it and then draw the predicted
+    # label on the image	image = cv2.merge([image] * 3)
+    image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_LINEAR)
+    cv2.putText(image, label, (5, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, color, 2)
 
-	# add the image to our list of output images
-	images.append(image)
+    # add the image to our list of output images
+    images.append(image)
 
 # construct the montage for the images
 montage = build_montages(images, (96, 96), (7, 7))[0]
